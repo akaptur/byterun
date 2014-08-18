aa119648240f0d143ab8b9b5a01e62e1bcfba6e4
Allison Kaptur <allison.kaptur@gmail.com>
2014-08-18 18:07:01 -0400

    A rough draft of the visualizer and a sample script to run it on

--- byterun/draw.py
+++ byterun/draw.py
@@ -2,53 +2,76 @@ import os
 import time
 import opcode
 
+EMPTY_LINE = " " * 10
+
 def draw_frame(frame,pad=14):
     """ Return an ascii-art frame representation, suitable for stacking."""
     template = """\
-| Frame {0.f_code.co_name:{fill}{align}{pad}}| -> blocks: {0.stack}
-|                     | -> data: {0.block_stack}
-"""
+| Frame {0.f_code.co_name:{fill}{align}{pad}}| -> blocks: {0.block_stack}
+|                     | -> data: {0.stack}"""
     return template.format(frame, align="<", fill=" ", pad=pad)
 
-def draw_bytes(frame):
+def draw_bytes(vm):
     """ Return the bytecode currently executing,
     with the active byte bolded."""
-    bytecode = frame.f_code.co_code
+
     out = []
+    for frame in vm.frames:
+        if frame.f_back and frame.f_code is frame.f_back.f_code:
+            continue
+        out.append("Frame {}".format(frame.f_code.co_name))
+        bytecode = frame.f_code.co_code
+        i = 0
+        while i < len(bytecode):
+            if i == frame.f_lasti:
+                fmt = "\x1b[31;1m{0}\x1b[39;0m"
+            else:
+                fmt = "\x1b[39;0m{}\x1b[39;0m"
+                # fmt = "{0}"
+            byteval = ord(bytecode[i])
+            out.append(fmt.format(opcode.opname[byteval]))
+            if byteval >= opcode.HAVE_ARGUMENT:
+                i += 3
+            else:
+                i += 1
+        out.append(EMPTY_LINE)
 
-    i = 0
-    while i < len(bytecode):
-        if i == frame.f_lasti:
-            fmt = "\x1b[31;1m{}\x1b[39;49m"
-        else:
-            fmt = "{}"
-        byteval = ord(bytecode[i])
-        out.append(fmt.format(opcode.opname[byteval]))
-        if byteval >= opcode.HAVE_ARGUMENT:
-            i += 3
-        else:
-            i += 1
-
-    return out
+    return "\n".join(out)
 
 
 def draw_vm(vm):
     divider = " --------------------- "
     out = [divider]
-    for frame in reversed(vm.frames):
+    for frame in vm.frames:
         out.append(draw_frame(frame))
         out.append(divider)
-    out.append(divider)
-    return out
+    return "\n".join(out)
 
 def two_cols(seq1, seq2, pad=30):
     """ Return two-column view of two texts,
-    given each text as a sequence"""
-    template = "{0:{pad}}    {1:{pad}}"
+    given each text as a string"""
+    # template = "{0:{fill}{pad}}{1:{fill}{pad}}"
+    template = "{}\x1b[30G{}"
     out = []
-    for linea, lineb in zip(seq1, seq2):
-        out.append(template.format(linea, lineb, pad=pad))
+    seq1 = seq1.split("\n")
+    seq2 = seq2.split("\n")
+
+    for linea, lineb in longzip(seq1, seq2):
+        print(repr(linea), repr(lineb))
+        out.append(template.format(linea.ljust(30), lineb.ljust(30)))
     return "\n".join(out)
+
+def longzip(seq1, seq2):
+    """ Zip lines together, running until both lists are exhausted"""
+    padding = abs(len(seq1) - len(seq2))
+    if len(seq1) > len(seq2):
+        seq2 += [EMPTY_LINE] * padding
+    else:
+        seq1 += [EMPTY_LINE] * padding
+
+    for pair in zip(seq1, seq2):
+        yield pair
+
 
 def empty():
     while True:
@@ -56,8 +79,8 @@ def empty():
 
 def render_vm(vm):
     current_stack = draw_vm(vm)
-    current_byte = draw_bytes(vm.frame)
-    current_state = two_cols(current_stack, current_byte)
+    current_byte = draw_bytes(vm)
+    current_state = two_cols(current_byte, current_stack)
     print("\x1b[2J\x1b[H")
     print(current_state)
-    time.sleep(0.2)
+    time.sleep(0.3)
